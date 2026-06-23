6b9ba1fb0a865542496b866e84057d6b36a042c6
Felipe Indejejczak <felipe.indejejczak@outlook.com>
2026-06-23 14:48:53 -0300

    Delete js directory

--- js/app.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  // Chama a função do state.js que puxa os arquivos e DEPOIS carrega a UI
-  initApp(); 
-  console.log('LocalInsta Interface Initialized - Conectado ao Python!');
-});
